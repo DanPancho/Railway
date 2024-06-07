b007e89dbb4c0dcc728025fc52cda037ece338f6
DanPancho <daarcosa@puce.edu.ec>
2024-06-07 01:35:27 -0500

    push init

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '¡Hola, Mundo!'
+    return '¡Hola, Mundo!, variable: ' + os.environ.get('NAME', 'DANIEL')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
